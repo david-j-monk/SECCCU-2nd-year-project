321f6dfed7413387e45e8f89fa12f3abc81ad84a
David Monk <d.monk584@canterbury.ac.uk>
2019-02-12 12:13:53 +0000

    added test db connection

--- SECCCU/SECCCU/Form1.cs
+++ SECCCU/SECCCU/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,43 @@ namespace SECCCU
         public Form1()
         {
             InitializeComponent();
+            try 
+            { 
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "secccuserver.database.windows.net"; 
+                builder.UserID = "secccuadmin";            
+                builder.Password = "SeccuPass1337$!";     
+                builder.InitialCatalog = "secccusql";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n");
+
+                    connection.Open();       
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("DROP TABLE Persons;");
+
+                                        
+                    String sql = sb.ToString();
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        //using (SqlDataReader reader = command.ExecuteReader())
+                        //{
+                        //    while (reader.Read())
+                        //    {
+                        //        Console.WriteLine("{0}", reader.GetString(0));
+                        //    }
+                        //}
+                    }                    
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadLine();
         }
     }
 }
