f30a847fa616df7212cc0017e711929c72cc2805
David Monk <d.monk584@canterbury.ac.uk>
2019-02-25 13:38:09 +0000

    Fixed database init

--- SECCCU/SECCCU/Database.cs
+++ SECCCU/SECCCU/Database.cs
@@ -39,29 +39,28 @@ namespace SECCCU
         {
             bool success = true;
 
-            Console.WriteLine("\nQuery data example:");
+            Console.WriteLine("\nStart database initialization DEBUG:");
             Console.WriteLine("=========================================\n");
 
-            Debug.WriteLine("CODE: Dropping and creating database tables.");
 
             StringBuilder sb = new StringBuilder();
 
             Debug.WriteLine("CODE: Dropping tables");
 
-            sb.Append("DROP TABLE IF EXISTS LECTURE;");
-            sb.Append("DROP TABLE IF EXISTS LECTURER;");
-            sb.Append("DROP TABLE IF EXISTS SCANNER;");
-            sb.Append("DROP TABLE IF EXISTS ROOM;");
-            sb.Append("DROP TABLE IF EXISTS STUDENT;");
-            sb.Append("DROP TABLE IF EXISTS PROGRAMME;");
+            sb.Append("DROP TABLE IF EXISTS LECTURES;");
+            sb.Append("DROP TABLE IF EXISTS LECTURERS;");
+            sb.Append("DROP TABLE IF EXISTS SCANNERS;");
+            sb.Append("DROP TABLE IF EXISTS ROOMS;");
             sb.Append("DROP TABLE IF EXISTS LOG;");
+            sb.Append("DROP TABLE IF EXISTS STUDENTS;");
+            sb.Append("DROP TABLE IF EXISTS PROGRAMMES;");
 
             success = SendQueryToDatabase(sb.ToString());
             if (!success) return success;
             sb = new StringBuilder();
 
-            Debug.WriteLine("CODE: Dropping tables");
-            sb.Append("CREATE TABLE programme(");
+            Debug.WriteLine("CODE: CREATE TABLE programmes");
+            sb.Append("CREATE TABLE programmes(");
             sb.Append("programme_id		char(9) 	    PRIMARY KEY,");
             sb.Append("programme_name	varchar(40)	    NOT NULL");
             sb.Append(")");
@@ -71,8 +70,8 @@ namespace SECCCU
 
             sb = new StringBuilder();
 
-            Debug.WriteLine("CODE: Dropping tables");
-            sb.Append("CREATE TABLE lecturer(");
+            Debug.WriteLine("CODE: CREATE TABLE lecturers");
+            sb.Append("CREATE TABLE lecturers(");
             sb.Append("lecturer_id 	    char(12)		PRIMARY KEY,");
             sb.Append("surname 		    varchar (40)	NOT NULL,");
             sb.Append("first_name	    varchar (40)	NOT NULL");
@@ -83,8 +82,8 @@ namespace SECCCU
 
             sb = new StringBuilder();
 
-            Debug.WriteLine("CODE: Dropping tables");
-            sb.Append("CREATE TABLE room(");
+            Debug.WriteLine("CODE: CREATE TABLE rooms");
+            sb.Append("CREATE TABLE rooms(");
             sb.Append("room_id 	        int             IDENTITY(1,1)    PRIMARY KEY,");
             sb.Append("room_name 		varchar (5) 	UNIQUE              NOT NULL");
             sb.Append(")");
@@ -94,10 +93,10 @@ namespace SECCCU
 
             sb = new StringBuilder();
 
-            Debug.WriteLine("CODE: Dropping tables");
-            sb.Append("CREATE TABLE scanner(");
+            Debug.WriteLine("CODE: CREATE TABLE scanners");
+            sb.Append("CREATE TABLE scanners(");
             sb.Append("scanner_id 	    int             IDENTITY(1,1)    PRIMARY KEY,");
-            sb.Append("room_id 		    int	            FOREIGN KEY      REFERENCES room(room_id)");
+            sb.Append("room_id 		    int	            FOREIGN KEY      REFERENCES rooms(room_id)");
             sb.Append(")");
 
             success = SendQueryToDatabase(sb.ToString());
@@ -105,15 +104,15 @@ namespace SECCCU
 
             sb = new StringBuilder();
 
-            Debug.WriteLine("CODE: Dropping tables");
-            sb.Append("CREATE TABLE lecture(");
+            Debug.WriteLine("CODE: CREATE TABLE lectures");
+            sb.Append("CREATE TABLE lectures(");
             sb.Append("lecture_id 	    int	            IDENTITY(1,1)   PRIMARY KEY,");
             sb.Append("lecture_name	    char(40)                ,");
             sb.Append("lecture_start    time		    NOT NULL,");
             sb.Append("lecture_end	    time		    NOT NULL,");
-            sb.Append("location	        int             FOREIGN KEY     REFERENCES      room(room_id),");
-            sb.Append("programme_id	    char(9)		    FOREIGN KEY     REFERENCES      programme(programme_id),");
-            sb.Append("lecturer_id	    char(12)		FOREIGN KEY     REFERENCES      lecturer(lecturer_id)");
+            sb.Append("location	        int             FOREIGN KEY     REFERENCES      rooms(room_id),");
+            sb.Append("programme_id	    char(9)		    FOREIGN KEY     REFERENCES      programmes(programme_id),");
+            sb.Append("lecturer_id	    char(12)		FOREIGN KEY     REFERENCES      lecturers(lecturer_id)");
             sb.Append(")");
 
             success = SendQueryToDatabase(sb.ToString());
@@ -121,12 +120,12 @@ namespace SECCCU
 
             sb = new StringBuilder();
 
-            Debug.WriteLine("CODE: Dropping tables");
-            sb.Append("CREATE TABLE student(");
+            Debug.WriteLine("CODE: CREATE TABLE students");
+            sb.Append("CREATE TABLE students(");
             sb.Append("student_id	    char(12)        PRIMARY KEY,");
             sb.Append("surname	        varchar(40)	    NOT NULL,");
             sb.Append("first_name       varchar(40)	    NOT NULL,");
-            sb.Append("programme_id	    char(9)		    FOREIGN KEY     REFERENCES      programme(programme_id),");
+            sb.Append("programme_id	    char(9)		    FOREIGN KEY     REFERENCES      programmes(programme_id),");
             sb.Append("CONSTRAINT       CK_first_name_Length            CHECK (LEN(first_name) >= 3),");
             sb.Append("CONSTRAINT       CK_surname_Length               CHECK (LEN(surname) >= 3)");
             sb.Append(");");
@@ -136,10 +135,10 @@ namespace SECCCU
 
             sb = new StringBuilder();
 
-            Debug.WriteLine("CODE: Dropping tables");
+            Debug.WriteLine("CODE: CREATE TABLE log");
             sb.Append("CREATE TABLE log(");
             sb.Append("log_id           int	            PRIMARY KEY     IDENTITY(1,1),");
-            sb.Append("student_id       char(12)        FOREIGN KEY     REFERENCES student(student_id),");
+            sb.Append("student_id       char(12)        FOREIGN KEY     REFERENCES students(student_id),");
             sb.Append("scan_time	    datetime2		NOT NULL");
             sb.Append(");");
 
@@ -149,11 +148,11 @@ namespace SECCCU
             sb = new StringBuilder();
 
             //Programmes to DB
-            Debug.WriteLine("CODE: Dropping tables");
+            Debug.WriteLine("CODE: Programmes to DB");
             string[][] rows = File.ReadAllLines("csvFiles\\programmes.csv").Select(l => l.Split(',').ToArray()).ToArray();
             for (int i = 0; i < rows.GetLength(0); i++)
             {
-                sb.Append("INSERT INTO programme (programme_id, programme_name)");
+                sb.Append("INSERT INTO programmes (programme_id, programme_name)");
                 sb.Append($"VALUES ('{rows[i][0]}', '{rows[i][1]}');");
             }
 
@@ -163,11 +162,11 @@ namespace SECCCU
             sb = new StringBuilder();
 
             //Lecturers to DB
-            Debug.WriteLine("CODE: Dropping tables");
-            rows = File.ReadAllLines("csvFiles\\lecturer.csv").Select(l => l.Split(',').ToArray()).ToArray();
+            Debug.WriteLine("CODE: Lecturers to DB");
+            rows = File.ReadAllLines("csvFiles\\lecturers.csv").Select(l => l.Split(',').ToArray()).ToArray();
             for (int i = 0; i < rows.GetLength(0); i++)
             {
-                sb.Append("INSERT INTO lecturer (lecturer_id, surname, first_name)");
+                sb.Append("INSERT INTO lecturers (lecturer_id, surname, first_name)");
                 sb.Append($"VALUES ('{rows[i][0]}', '{rows[i][1]}', '{rows[i][2]}');");
             }
 
@@ -177,7 +176,7 @@ namespace SECCCU
             sb = new StringBuilder();
 
             //Rooms to DB
-            Debug.WriteLine("CODE: Dropping tables");
+            Debug.WriteLine("CODE: Rooms to DB");
             rows = File.ReadAllLines("csvFiles\\rooms.csv").Select(l => l.Split(',').ToArray()).ToArray();
             for (int i = 0; i < rows.GetLength(0); i++)
             {
@@ -192,11 +191,11 @@ namespace SECCCU
             sb = new StringBuilder();
             
             //Students to DB
-            Debug.WriteLine("CODE: Dropping tables");
+            Debug.WriteLine("CODE: Students to DB");
             rows = File.ReadAllLines("csvFiles\\students.csv").Select(l => l.Split(',').ToArray()).ToArray();
             for (int i = 0; i < rows.GetLength(0); i++)
             {
-                sb.Append("INSERT INTO student (surname, first_name, student_id, programme_id)");
+                sb.Append("INSERT INTO students (surname, first_name, student_id, programme_id)");
                 sb.Append($"VALUES ('{rows[i][0]}', '{rows[i][1]}','{rows[i][2]}','{rows[i][3]}');");
             }
 
@@ -206,7 +205,7 @@ namespace SECCCU
             sb = new StringBuilder();
 
             //Scanners to DB
-            Debug.WriteLine("CODE: Dropping tables");
+            Debug.WriteLine("CODE: Scanners to DB");
             rows = File.ReadAllLines("csvFiles\\scanners.csv").Select(l => l.Split(',').ToArray()).ToArray();
             for (int i = 0; i < rows.GetLength(0); i++)
             {
@@ -220,39 +219,16 @@ namespace SECCCU
             sb = new StringBuilder();
 
             //Lectures to DB
-            Debug.WriteLine("CODE: Dropping tables");
+            Debug.WriteLine("CODE: Lectures to DB");
             rows = File.ReadAllLines("csvFiles\\lectures.csv").Select(l => l.Split(',').ToArray()).ToArray();
             for (int i = 0; i < rows.GetLength(0); i++)
             {
-                sb.Append("INSERT INTO lecture (lecture_name, lecture_start, lecture_end, location, programme_id, lecturer_id)");
+                sb.Append("INSERT INTO lectures (lecture_name, lecture_start, lecture_end, location, programme_id, lecturer_id)");
                 sb.Append($"VALUES ('{rows[i][0]}', '{rows[i][1]}', '{rows[i][2]}', '{rows[i][3]}', '{rows[i][4]}', '{rows[i][5]}');");
             }
 
             success = SendQueryToDatabase(sb.ToString());
             return success;
-
-
-            //string query = sb.ToString();
-            //try
-            //{
-            //    int rowsAffected;
-            //    using (SqlCommand command = new SqlCommand(query, Connection))
-            //    {
-            //        Debug.WriteLine("CODE: Executing command");
-            //        Connection.Open();
-            //        rowsAffected = command.ExecuteNonQuery();
-            //        Connection.Close();
-            //    }
-            //    Debug.WriteLine("CODE: Command executed successfully \n \t" + rowsAffected + " Rows Affected!");
-            //    return true;
-
-            //}
-            //catch (Exception e)
-            //{
-            //    Debug.WriteLine("ERROR: " + e);
-            //    return false;
-            //}
-
         }
 
         private bool SendQueryToDatabase(string query)
@@ -262,7 +238,6 @@ namespace SECCCU
                 int rowsAffected;
                 using (SqlCommand command = new SqlCommand(query, Connection))
                 {
-                    Debug.WriteLine("CODE: Executing command");
                     Connection.Open();
                     rowsAffected = command.ExecuteNonQuery();
                     Connection.Close();
@@ -293,13 +268,13 @@ namespace SECCCU
                     command.ExecuteNonQuery();
                 }
 
-                using (SqlCommand command = new SqlCommand($"SELECT first_name, surname FROM student WHERE student_id = '{cardNumber}';", Connection))
+                using (SqlCommand command = new SqlCommand($"SELECT first_name, surname FROM students WHERE student_id = '{cardNumber}';", Connection))
                 {
                     SqlDataReader dataReader = command.ExecuteReader();
                     while (dataReader.Read())
                     {
                         returnString[0] = string.Format($"{dataReader.GetString(0)} {dataReader.GetString(1)}");
-                        returnString[1] = string.Format($"Swipe Success");
+                        returnString[1] = "Swipe Success";
                     }
                 }
             }
@@ -309,12 +284,12 @@ namespace SECCCU
                 switch (exception.Number)
                 {
                     case 547:
-                        returnString[0] =  String.Format("Card Read Error");
-                        returnString[1] =  String.Format("Error");
+                        returnString[0] =  "Card Read Error";
+                        returnString[1] =  "Error";
                         break;
                     case 8152:
-                        returnString[0] =  String.Format("Card Read Error");
-                        returnString[1] =  String.Format("Error");
+                        returnString[0] =  "Card Read Error";
+                        returnString[1] =  "Error";
                         break;
                     default:
                         throw;
